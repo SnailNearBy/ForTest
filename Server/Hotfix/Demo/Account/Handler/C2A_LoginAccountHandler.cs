b6a0de6437a8db13962478891377b9d371c4e98e
SnailNearBy <h1968663814@163.com>
2022-11-11 18:35:22 +0800

    登陆逻辑修改

---
+++ Server/Hotfix/Demo/Account/Handler/C2A_LoginAccountHandler.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace ET
+{
+    [FriendClass(typeof(Account))]
+    public  class C2A_LoginAccountHandler : AMRpcHandler<C2A_LoginAccount,A2C_LoginAccount>
+    {
+        protected override async ETTask Run(Session session, C2A_LoginAccount request, A2C_LoginAccount response, Action reply)
+        {
+            if (session.DomainScene().SceneType != SceneType.Account)
+            {
+                Log.Error($"请求的Scene错误，当前的Scene为：{session.DomainScene().SceneType}");
+                session.Dispose();
+                return;
+            }
+            session.RemoveComponent<SessionAcceptTimeoutComponent>();
+
+            if (session.GetComponent<SessionLockingComponent>() != null)
+            {
+                response.Error = ErrorCode.ERR_RequestRepeatedly;
+                reply();
+                session.Disconnect().Coroutine();
+                return;
+            }
+            
+            if (string.IsNullOrEmpty(request.AccountName) || string.IsNullOrEmpty(request.Password))
+            {
+                response.Error = ErrorCode.ERR_LoginInfoIsNull;
+                reply();
+                session.Disconnect().Coroutine();
+                return;
+            } 
+
+            if (!Regex.IsMatch(request.AccountName.Trim(), @"^(?=.*[0-9].*)(?=.*[A-Z].*)(?=.*[a-z].*).{6,15}$"))
+            {
+                response.Error = ErrorCode.ERR_AccountNameFormError;
+                reply();
+                session.Disconnect().Coroutine();
+                return;
+            }
+            
+            if (!Regex.IsMatch(request.Password.Trim(), @"^(?=.*[0-9].*)(?=.*[A-Z].*)(?=.*[a-z].*).{6,15}$"))
+            {
+                response.Error = ErrorCode.ERR_PasswordFormError;
+                reply();
+                session.Disconnect().Coroutine();
+                return;
+            }
+
+            using (session.AddComponent<SessionLockingComponent>())
+            {
+                //使用协程锁防止多个玩家同时注册同一个账号导致数据错乱
+                using (await CoroutineLockComponent.Instance.Wait(CoroutineLockType.LoginAccount,request.AccountName.Trim().GetHashCode()))
+                {
+                    
+                    var accountInfoList = await DBManagerComponent.Instance.GetZoneDB(session.DomainZone())
+                            .Query<Account>(d => d.AccountName.Equals(request.AccountName.Trim()));
+                    Account account = null;
+                    if (accountInfoList != null && accountInfoList.Count > 0)
+                    {
+                        account = accountInfoList[0];
+                        session.AddChild(account);
+                        if (account.AccountType == (int)AccountType.BlackList)
+                        {
+                            response.Error = ErrorCode.ERR_AccountInBlackListError;
+                            reply();
+                            session.Disconnect().Coroutine();
+                            account?.Dispose();
+                            return;
+                        }
+
+                        if (!account.Password.Equals(request.Password))
+                        {
+                            response.Error = ErrorCode.ERR_LoginPasswordError;
+                            reply();
+                            session.Disconnect().Coroutine();
+                            account?.Dispose();
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        account = session.AddChild<Account>();
+                        account.AccountName = request.AccountName.Trim();
+                        account.Password = request.Password;
+                        account.CreateTime = TimeHelper.ServerNow();
+                        account.AccountType = (int)AccountType.General;
+                        await DBManagerComponent.Instance.GetZoneDB(session.DomainZone()).Save(account);
+
+                    }
+
+                    string Token = TimeHelper.ServerNow().ToString() + RandomHelper.RandomNumber(int.MinValue, int.MaxValue);
+                    session.DomainScene().GetComponent<TokenComponent>().Remove(account.Id);
+                    session.DomainScene().GetComponent<TokenComponent>().Add(account.Id, Token);
+
+                    response.AccountId = account.Id;
+                    response.Token = Token;
+                    
+                    reply();
+                    account?.Dispose();
+                    //await ETTask.CompletedTask;
+                }
+            }
+        }
+    }
+}
---
+++ Server/Hotfix/Demo/Login/C2M_TestActorLocationMessageHandler.cs
@@ -0,0 +1,14 @@
+namespace ET
+{
+    [ActorMessageHandler]
+    public class C2M_TestActorLocationMessageHandler : AMActorLocationHandler<Unit,C2M_TestActorLocationMessage>
+    {
+        protected override async ETTask Run(Unit unit, C2M_TestActorLocationMessage message)
+        {
+            Log.Debug(message.Content);
+            
+            MessageHelper.SendToClient(unit,new M2C_TestActorMessage(){Content = "333333"});
+            await ETTask.CompletedTask;
+        }
+    }
+}
